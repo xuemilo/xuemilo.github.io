c306075a06b74210188646116cb9f43956acbd55
Xue <browniexueting@gmail.com>
2025-08-03 23:08:30 +0800

    Update main.js

--- webdevA2/main.js
+++ webdevA2/main.js
@@ -356,3 +356,31 @@ function CheckAns() {
 
   scorebox.innerHTML = "Score: " + score;
 }
+
+/* full screen */
+const btnFS = document.querySelector("#btnFS");
+const btnWS = document.querySelector("#btnWS");
+btnFS.addEventListener("click", enterFullscreen);
+btnWS.addEventListener("click", exitFullscreen);
+function enterFullscreen() { //must be called by user generated event
+  if (document.documentElement.requestFullscreen) {
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+    document.documentElement.webkitRequestFullscreen();
+  } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+    document.documentElement.msRequestFullscreen();
+  }
+}
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { // Firefox
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { // IE/Edge
+    document.msExitFullscreen();
+  }
+}
